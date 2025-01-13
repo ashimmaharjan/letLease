6aad3a09aa246f8d4f9bcbe7d6a4c65305861e48
Ashim Maharjan <ciferulquiorra008@gmail.com>
2025-01-13 12:24:50 +1100

    Data seed function added and based on those seeded data, homepage populated with actual dynamic data and filter functionality added via category

--- constants/lib/appwrite.ts
+++ constants/lib/appwrite.ts
@@ -1,4 +1,11 @@
-import { Avatars, Account, Client, OAuthProvider } from "react-native-appwrite";
+import {
+  Avatars,
+  Account,
+  Client,
+  OAuthProvider,
+  Databases,
+  Query,
+} from "react-native-appwrite";
 import { openAuthSessionAsync } from "expo-web-browser";
 import * as Linking from "expo-linking";
 
@@ -6,6 +13,13 @@ export const config = {
   platform: "com.ashim.letlease",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -17,23 +31,25 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const databases = new Databases(client);
 
 export async function login() {
   try {
     const redirectUri = Linking.createURL("/");
     const response = await account.createOAuth2Token(
       OAuthProvider.Google,
-      redirectUri
+      redirectUri,
     );
 
     if (!response) throw new Error("Failed t login.");
 
     const browserResult = await openAuthSessionAsync(
       response.toString(),
-      redirectUri
+      redirectUri,
     );
 
-    if (browserResult.type !== "success") throw new Error("Create OAuth2Token failed.");
+    if (browserResult.type !== "success")
+      throw new Error("Create OAuth2Token failed.");
 
     const url = new URL(browserResult.url);
     const secret = url.searchParams.get("secret")?.toString();
@@ -77,4 +93,63 @@ export async function getCurrentUser() {
     console.error(error);
     return null;
   }
+}
+
+export async function getLatestProperties() {
+  try {
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      [Query.orderAsc('$createdAt'), Query.limit(5)]
+    )
+
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getProperties({filter, query, limit}: {
+  filter: string;
+  query: string;
+  limit?: number;
+}) {
+ try {
+  const buildQuery = [Query.orderAsc('$createdAt')];
+  
+  if(filter && filter !== 'All') {
+    buildQuery.push(Query.equal('propertyType', filter));
+  }
+
+  if(query) {
+    buildQuery.push(
+      Query.or(
+        [
+          Query.search('name', query),
+          Query.search('address', query),
+          Query.search('propertyType', query)
+        ]
+      )
+    )
+  }
+
+  if(limit){
+    buildQuery.push(Query.limit(limit));
+  }
+
+  const result = await databases.listDocuments(
+    config.databaseId!,
+    config.propertiesCollectionId!,
+    buildQuery,
+  )
+
+  return result.documents;
+  
+ } catch (error) {
+  console.error(error);
+  return [];
+ }
+
+
 }
